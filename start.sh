79d2adfd72bfb6af46ba06c294ddbd123ffbbd29
phoenix <1797318+phoenixxie0@users.noreply.github.com>
2022-06-29 21:31:01 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh 
-if [ ! -d "~/mcl" ]; then
-  mkdir -p ~/mcl
-  cd ~/mcl 
+if [ ! -d "/root/mcl" ]; then
+  mkdir -p /root/mcl
+  cd /root/mcl 
   wget https://github.com/iTXTech/mirai-console-loader/releases/download/v2.1.0/mcl-2.1.0.zip 
   unzip mcl-2.1.0.zip 
   chmod +x mcl 
@@ -9,7 +9,7 @@ if [ ! -d "~/mcl" ]; then
   echo -e "mcl安装完成...\n"
 fi
 
-cd ~/mcl 
-nohup ./mcl &>/dev/null
+cd /root/mcl 
+nohup ./mcl &>/dev/null &
 echo -e "mcl启动完成...\n"
 ehforwarderbot
