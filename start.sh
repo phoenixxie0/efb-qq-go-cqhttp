f19eb5f5731decfac647bcb23d74173b3083c507
phoenix <1797318+phoenixxie0@users.noreply.github.com>
2023-04-25 11:47:06 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ ! -f "/root/go-cqhttp/go-cqhttp" ]; then
   mkdir -p /root/go-cqhttp
   cd /root/go-cqhttp 
-  wget https://github.com/Mrs4s/go-cqhttp/releases/download/v1.0.0/go-cqhttp_linux_amd64.tar.gz
+  wget https://github.com/Mrs4s/go-cqhttp/releases/download/v1.0.1/go-cqhttp_linux_amd64.tar.gz
   tar -xvf go-cqhttp_linux_amd64.tar.gz 
   chmod +x go-cqhttp 
   echo -e "go-cqhttp安装完成...\n"
@@ -11,6 +11,6 @@ fi
 cd /root/go-cqhttp 
 nohup ./go-cqhttp &>/dev/null &
 echo -e "go-cqhttp启动完成...\n"
-sleep 10
+sleep 20
 ehforwarderbot &
 sh
